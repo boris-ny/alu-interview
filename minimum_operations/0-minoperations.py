4ae4bdbd0a3172bc6aec6cef638e3310e81643be
boris-ny <b.nyilindek@alustudent.com>
2023-05-25 12:53:46 +0200

    Minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,30 +1,20 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
 def minOperations(n):
-  """
-  Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    if n == 1:
+        return 0  # No operations needed, already have 1 H
 
-  Args:
-    n: The number of H characters to be in the file.
+    operations = [float('inf')] * (n + 1)
+    operations[1] = 0
 
-  Returns:
-    The fewest number of operations needed.
+    for i in range(2, n + 1):
+        j = 2
+        while j * j <= i:
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + i // j)
+                operations[i] = min(operations[i], operations[i // j] + j)
+            j += 1
 
-  Raises:
-    Exception: If n is impossible to achieve.
-  """
-
-  # Check if n is possible to achieve.
-  if n <= 0:
-    raise Exception("n must be positive")
-
-  # Initialize the dp table.
-  dp = [0] * (n + 1)
-
-  # Fill in the dp table.
-  for i in range(1, n + 1):
-    # The minimum number of operations to get i H characters is the minimum of the following:
-    #   * The minimum number of operations to get i - 1 H characters, plus 1 operation to insert a new H character.
-    #   * The minimum number of operations to get i / 2 H characters, plus 1 operation to copy all the characters and paste them again.
-    dp[i] = min(dp[i - 1] + 1, dp[i // 2] + 1)
-
-  # Return the minimum number of operations.
-  return dp[n]
+    return operations[n] if operations[n] != float('inf') else 0
