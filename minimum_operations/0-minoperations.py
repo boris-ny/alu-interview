1c3dc3e7adbdee8f3c8d2e6ef26d3204a6a2bbca
boris-ny <b.nyilindek@alustudent.com>
2023-05-25 14:24:05 +0200

    mini

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-
-'''Minimum operations'''
+"""Minimum Operations"""
 
 
 def minOperations(n):
-    if n == 1:
-        return 0
+    """Calculation  of operations needed to obtain exactly n H characters
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+    """
+
+    if n <= 1:
+       return 0
 
     operations = 0
-    divisor = 2
-
-    while n > 1:
-        if n % divisor == 0:
-            n //= divisor
-            operations += divisor
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            operations += i
+            n //= i
         else:
-            divisor += 1
+            i += 1
 
     return operations
