764bd60837e92fa6aaf22cc73ef5dcff4a48d2b9
boris-ny <b.nyilindek@alustudent.com>
2023-06-15 15:42:39 +0200

    rain file

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,9 +3,11 @@
 Prototype: def rain(walls)
 walls is a list of non-negative integers.
 Return: Integer indicating total amount of rainwater retained.
-Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water.
+Assume that the ends of the list (before index 0 and after index walls[-1]) are
+not walls, meaning they will not retain water.
 If the list is empty return 0.
 """
+
 
 def rain(walls):
     if len(walls) < 3:
